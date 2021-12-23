8d862f0058dc5e6a403966d7414a302b5fc81344
Sartain, Alex <alex.sartain@zuhlke.com>
2021-12-23 17:35:32 +0000

    Add fold on x index

--- src/Advent13.kt
+++ src/Advent13.kt
@@ -6,6 +6,7 @@ import kotlin.math.max
 class Advent13 : Advent {
 
     var wrapArray : List<List<String>> = listOf()
+    var instructionList : List<String> = listOf()
 
     override fun part1() {
         TODO("Not yet implemented")
@@ -19,6 +20,12 @@ class Advent13 : Advent {
         var maxX = 0
         var maxY = 0
         for(instruction in inputList) {
+            if(instruction.startsWith("fold")) {
+                val mutableInstructionList = instructionList.toMutableList()
+                mutableInstructionList.add(instruction)
+                instructionList = mutableInstructionList
+                break
+            }
             val xCoord : Int = Integer.valueOf(instruction.split(",")[0])
             val yCoord : Int = Integer.valueOf(instruction.split(",")[1])
             if(xCoord > maxX)
@@ -37,6 +44,9 @@ class Advent13 : Advent {
         }
         wrapArray = mutableWrapArray
         for(instruction in inputList) {
+            if(instruction.startsWith("fold")) {
+                break
+            }
             val xCoord : Int = Integer.valueOf(instruction.split(",")[0])
             val yCoord : Int = Integer.valueOf(instruction.split(",")[1])
             var mutableRow = mutableWrapArray[yCoord].toMutableList()
@@ -44,5 +54,42 @@ class Advent13 : Advent {
             mutableWrapArray[yCoord] = mutableRow
         }
         wrapArray = mutableWrapArray
+    }
+
+    fun followInstructionsUpTo(instructionMax : Int) {
+        for(i in 0..instructionMax-1) {
+            followInstruction(instructionList[i])
+        }
+    }
+
+    private fun followInstruction(instruction : String) {
+        val axisToFold : String = instruction.split("fold along ")[1].split("=")[0]
+        val positionToFold : Int = Integer.valueOf(instruction.split("=")[1])
+        if(axisToFold == "x")
+            performXFold(positionToFold)
+    }
+
+    fun performXFold(valueToFoldOn : Int) {
+        //We want to split the array in half with values before and after the fold
+        var mutableWrapArray = wrapArray.toMutableList()
+        for(y in 0..wrapArray.lastIndex) {
+            for(x in 0..valueToFoldOn) {
+                if (wrapArray[y][x] == "#" || wrapArray[y][(valueToFoldOn * 2) - x] == "#") {
+                    var mutableRow = mutableWrapArray[y].toMutableList()
+                    mutableRow[x] = "#"
+                    mutableWrapArray[y] = mutableRow
+                } else {
+                    var mutableRow = mutableWrapArray[y].toMutableList()
+                    mutableRow[x] = "."
+                    mutableWrapArray[y] = mutableRow
+                }
+            }
+        }
+        //Now split the array
+        var splitMutableWrapArray = mutableWrapArray
+        for(y in 0..wrapArray.lastIndex) {
+            splitMutableWrapArray[y] = mutableWrapArray[y].dropLast(valueToFoldOn + 1)
+        }
+        wrapArray = splitMutableWrapArray
     }
 }
--- test/Advent13Test.kt
+++ test/Advent13Test.kt
@@ -12,4 +12,12 @@ class Advent13Test {
         advent13.receiveInput(listOf("2,1"))
         assertEquals(listOf(listOf(".", ".", "."), listOf(".", ".", "#")), advent13.wrapArray)
     }
+
+    @Test
+    fun foldArrayGivenAppendedInstructions() {
+        val advent13 = Advent13()
+        advent13.receiveInput(listOf("2,1", "fold along x=1"))
+        advent13.followInstructionsUpTo(1)
+        assertEquals(listOf(listOf("."), listOf("#")), advent13.wrapArray)
+    }
 }
